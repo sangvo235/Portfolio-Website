9a7f4f8845ee33834d16d5981bc1f88cd84ad86f
Sang Vo <97276811+sangvo235@users.noreply.github.com>
2023-06-18 03:26:04 -0700

    Add files via upload

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Sang Vo',
+  description: "Sang Vo's Personal Portfolio",
+  icons: {
+    icon: '/favicon.ico?v=4',
+  },
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Sang Vo Personal Portfolio</title>
+        <meta name="description" content="Sang Vo Personal Portfolio" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    <main>
+      <h1>hello</h1>
+    </main>
+    </div>
+  )
+}
